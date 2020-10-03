d377d84f3708979f3e07bcc887176361cecc7418
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-03 20:02:47 +0200

    fix problem with modal

--- src/js/Modal.js
+++ src/js/Modal.js
@@ -1,32 +1,26 @@
-export {
-    Modal
-};
+export { Modal };
 class Modal {
-    constructor() {
-        const body = document.querySelector("body")
-        this.div = document.createElement("div")
-        this.h1 = document.createElement("h1")
-        this.div.classList.add("modalDiv")
-        this.h1.classList.add("modalH1")
-        body.appendChild(this.div)
-        this.div.appendChild(this.h1)
-        this.time = 3000
+  constructor() {
+    const body = document.querySelector("body");
+    this.div = document.createElement("div");
+    this.h1 = document.createElement("h1");
+    this.div.classList.add("modalDiv");
+    this.h1.classList.add("modalH1");
+    body.appendChild(this.div);
+    this.div.appendChild(this.h1);
+    this.time = 3000;
+  }
+
+  displayModal(winner) {
+    if (winner == "-") {
+      this.h1.innerHTML = `Hard game! Draw`;
+    } else {
+      this.h1.innerHTML = `Congratulations!!! Player <span>${winner}</span> won`;
     }
+    this.div.style.display = "flex";
 
-
-
-    displayModal(winner) {
-        if (winner == "-") {
-            this.h1.textContent = `Hard game! Draw`
-        } else {
-            this.h1.textContent = `Congratulations!!! Player won: ${winner}`
-        }
-        this.div.style.display = "flex"
-
-        setTimeout(
-            () => {
-                this.div.style.display = "none"
-            }, this.time
-        )
-    }
+    setTimeout(() => {
+      this.div.style.display = "none";
+    }, this.time);
+  }
 }
