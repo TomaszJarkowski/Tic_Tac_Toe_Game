55c548a4f155dc830a12293186b88a0c7652d5d1
Tomasz Jarkowski <tomaszjarkowski1998@gmail.com>
2020-10-04 16:58:30 +0200

    create object with colors

---
+++ src/js/theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+  light: {
+    bgColor: "#F7F7F7",
+    bgColorHeader: "rgba(0,0,0,0.7)",
+    boardColor: "gray",
+    shadow: "0 0 7px 0 black",
+    fontColor: "black",
+    buttonBorder: "2px solid black",
+    bgColorButton: "white",
+  },
+  dark: {
+    bgColor: "#242526",
+    bgColorHeader: "#242526",
+    boardColor: "#18191A",
+    shadow: "0 0 7px 2px black",
+    fontColor: "white",
+    buttonBorder: "2px solid gray",
+    bgColorButton: "#18191A",
+  },
+};
--- src/js/toggle.js
+++ src/js/toggle.js
@@ -1,15 +1,15 @@
+import { theme } from "./theme";
+
 export const toggle = () => {
   const toggleLight = document.querySelector(".toggle__light");
   const toggleDark = document.querySelector(".toggle__dark");
   const conteiner = document.querySelector(".conteiner");
   const checktoggle = document.querySelector("#checktoggle");
   const board = document.querySelector(".board");
-  const boxs = document.querySelectorAll(".box");
   const info = document.querySelector(".info");
   const button = document.querySelector(".info button");
   const header = document.querySelector("header");
   const modal = document.querySelector(".modalDiv");
-  const modalH1 = document.querySelector(".modalH1");
   let dark = localStorage.getItem("dark");
   dark = JSON.parse(dark);
 
@@ -19,56 +19,82 @@ export const toggle = () => {
     changeColor(dark);
   });
 
+  const changeColorInHtml = (
+    toggleDarkOpacity,
+    toggleLightOpacity,
+    bgColor,
+    shadow,
+    boardColor,
+    fontColor,
+    buttonBorder,
+    bgColorButton,
+    bgColorHeader
+  ) => {
+    toggleLight.style.opacity = toggleLightOpacity;
+    toggleDark.style.opacity = toggleDarkOpacity;
+
+    conteiner.style.backgroundColor = bgColor;
+    document.body.style.backgroundColor = bgColor;
+
+    header.style.backgroundColor = bgColorHeader;
+    header.style.boxShadow = shadow;
+
+    board.style.backgroundColor = boardColor;
+    board.style.boxShadow = shadow;
+
+    modal.style.backgroundColor = boardColor;
+
+    info.style.backgroundColor = boardColor;
+    info.style.boxShadow = shadow;
+
+    button.style.backgroundColor = bgColorButton;
+    button.style.border = buttonBorder;
+    button.style.color = fontColor;
+  };
+
   const changeColor = (dark) => {
+    let toggleDarkOpacity,
+      toggleLightOpacity,
+      bgColor,
+      shadow,
+      boardColor,
+      fontColor,
+      buttonBorder,
+      bgColorButton,
+      bgColorHeader;
+
     if (dark) {
-      toggleLight.style.opacity = "0.5";
-      toggleDark.style.opacity = "1";
-      conteiner.style.backgroundColor = "#242526";
-      document.body.style.backgroundColor = "#242526";
-
-      header.style.backgroundColor = "#242526";
-      header.style.boxShadow = "0 0 7px 2px black";
-
-      board.style.backgroundColor = "#18191A";
-      board.style.boxShadow = "0 0 7px 2px black";
-
-      modal.style.backgroundColor = "#18191A";
-      modalH1.style.color = "white";
-
-      info.style.backgroundColor = "#18191A";
-      info.style.boxShadow = "0 0 7px 2px black";
-      info.style.color = "white";
-
-      button.style.backgroundColor = "#18191A";
-      button.style.border = "2px solid gray";
-      button.style.color = "white";
+      toggleDarkOpacity = "1";
+      toggleLightOpacity = "0.5";
+      bgColor = theme.dark.bgColor;
+      shadow = theme.dark.shadow;
+      boardColor = theme.dark.boardColor;
+      fontColor = theme.dark.fontColor;
+      buttonBorder = theme.dark.buttonBorder;
+      bgColorButton = theme.dark.bgColorButton;
+      bgColorHeader = theme.dark.bgColorHeader;
     } else {
-      toggleLight.style.opacity = "1";
-      toggleDark.style.opacity = "0.5";
-
-      conteiner.style.backgroundColor = "#F7F7F7";
-      document.body.style.backgroundColor = "#F7F7F7";
-
-      board.style.backgroundColor = "gray";
-      board.style.boxShadow = "0 0 7px 0 black";
-
-      header.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
-      header.style.boxShadow = "0 0 7px 0 black";
-
-      info.style.backgroundColor = "gray";
-      info.style.boxShadow = "0 0 7px 0px black";
-
-      modal.style.backgroundColor = "gray";
-      modalH1.style.color = "white";
-
-      button.style.backgroundColor = "white";
-      button.style.border = "2px solid black";
-      button.style.color = "black";
-
-      boxs.forEach((box) => {
-        box.style.boxShadow = "0 0 7px 0px black";
-      });
+      toggleDarkOpacity = "0.5";
+      toggleLightOpacity = "1";
+      bgColor = theme.light.bgColor;
+      shadow = theme.light.shadow;
+      boardColor = theme.light.boardColor;
+      fontColor = theme.light.fontColor;
+      buttonBorder = theme.light.buttonBorder;
+      bgColorButton = theme.light.bgColorButton;
+      bgColorHeader = theme.light.bgColorHeader;
     }
+    changeColorInHtml(
+      toggleDarkOpacity,
+      toggleLightOpacity,
+      bgColor,
+      shadow,
+      boardColor,
+      fontColor,
+      buttonBorder,
+      bgColorButton,
+      bgColorHeader
+    );
   };
   const changePosition = (flague) => {
     if (flague) {
